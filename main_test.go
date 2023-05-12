6ded6d260993fc3913681a0caa503b4e46dc10bb
marcomeulenbroeks <50397094+marcomeulenbroeks@users.noreply.github.com>
2023-05-12 14:37:19 +0200

    applied fmt

--- main_test.go
+++ main_test.go
@@ -6,8 +6,8 @@ import (
 
 func TestGetMessage(t *testing.T) {
 	expected := "Hello World!"
-    actual := GetMessage()
-    if actual != expected {
-       t.Errorf("GetMessage was incorrect, got: %v, want: %v.", actual, expected)
-    }
+	actual := GetMessage()
+	if actual != expected {
+		t.Errorf("GetMessage was incorrect, got: %v, want: %v.", actual, expected)
+	}
 }
